c16efd0ebcb0cfa4111b2fd95931d3fafc5fb9d4
Alexhpdv4 <alexlougog34@gmail.com>
2023-07-24 05:52:00 -0500

    autoclose windows

--- main.js
+++ main.js
@@ -36,16 +36,16 @@ const bannerTop = document.querySelector(".banner-top");
 // -------------------------------------------- products ---------------------------------------------
 // main-container
 const cardsContainer = document.querySelector('.cards-container');
-const productCard = document.querySelector('.product-card');
+// const productCards = document.querySelectorAll('.product-card');
 // -------------------------------------------- details ----------------------------------------------
 // product-detail=items
 const productDetalItem = document.querySelector(".product-detail-item");
 const productDetailsCl = document.querySelector(".product-detail-close");
 const imgSrc = document.querySelector(".img-src");
 const priceDetail = document.querySelector(".price-detail");
-const nameTitle = document.querySelector(".name-Title")
+const nameTitle = document.querySelector(".name-Title");
+const descriptionDetail = document.querySelector(".description-detail");
 const addToCartButton = document.querySelector(".add-to-cart-button");
-
 
 let amount = 0;
 let numItems = 0;
@@ -54,6 +54,51 @@ counter.innerHTML = numItems;
 // ######################################### Array & objects #########################################
 const productList = [];
 // objects products
+
+// Cells
+productList.push({
+  category: 'cells',
+  name: 'Xiaomi Celular Redmi',
+  price: 3834,
+  image: 'https://m.media-amazon.com/images/I/61LvXyda+6L._AC_SL1000_.jpg',
+  definition: "Xiaomi Celular Redmi Note 12S 8GB 256GB Negro Onix Desbloqueado 2303ERA42L"
+});
+productList.push({
+  category: 'cells',
+  name: 'Xiaomi Celular Poco',
+  price: 3333,
+  image: 'https://m.media-amazon.com/images/I/51aQ+dft2nL._AC_SL1000_.jpg',
+  definition: "Xiaomi Celular Poco M5s Blue 8GB Ram 256GB ROM"
+});
+productList.push({
+  category: 'cells',
+  name: 'Xiaomi Celular Redmi Note',
+  price: 3336,
+  image: 'https://m.media-amazon.com/images/I/51UhkQTNxmL._AC_SL1000_.jpg',
+  definition: "Xiaomi Celular Redmi Note 11S Graphite Gray 8Gb Ram 128Gb ROM"
+});
+productList.push({
+  category: 'cells',
+  name: 'SAMSUNG Galaxy M14 5G Azul',
+  price: 4999,
+  image: 'https://m.media-amazon.com/images/I/71ktEUgiLUL._AC_SL1500_.jpg',
+  definition: "SAMSUNG Galaxy M14 5G Azul"
+});
+productList.push({
+  category: 'cells',
+  name: 'XIAOMI REDMI 9A',
+  price: 1503,
+  image: 'https://m.media-amazon.com/images/I/71UHrexGN5L._AC_SL1500_.jpg',
+  definition: "XIAOMI REDMI 9A 2GB 32GB GRIS DESBLOQUEADO"
+});
+productList.push({
+  category: 'cells',
+  name: 'Apple iPhone 11',
+  price: 6590,
+  image: 'https://m.media-amazon.com/images/I/31PpUfTCiFL._AC_.jpg',
+  definition: "Apple iPhone 11, Totalmente Desbloqueado, 64GB, Negro (Reacondicionado)"
+});
+// Tools
 productList.push({
   category: 'tools',
   name: 'Separadora de pantalla',
@@ -84,65 +129,134 @@ productList.push({
   price: 267,
   image: 'https://m.media-amazon.com/images/I/61vAsvgA9VL._AC_SL1500_.jpg',
 });
+// Screens
 productList.push({
   category: 'screens',
-  name: 'Pantalla LCD',
-  price: 488,
-  image: 'https://m.media-amazon.com/images/I/61+I00feV9L._AC_SL1100_.jpg',
+  name: 'MEPED Pantalla LCD',
+  price: 429,
+  image: 'https://m.media-amazon.com/images/I/61aM4ckUnFL._AC_SL1000_.jpg',
+  definition: "MEPED Pantalla LCD Compatible con Huawei Y9 2019 Jkm-lx1 Lx2 Lx3 LCD, Display Digitalizador de Pantalla para Y9 2019 Jkm-lx1 Lx2 Lx3"
 });
 productList.push({
   category: 'screens',
-  name: 'Pantalla LCD',
-  price: 504,
-  image: 'https://m.media-amazon.com/images/I/51wU8SLM13L._AC_.jpg',
+  name: ' Pantalla táctil de repuesto',
+  price: 646.,
+  image: 'https://m.media-amazon.com/images/I/61fY-7iF27L._AC_SL1500_.jpg',
+  definition: "HYYT - Pantalla táctil de repuesto para Huawei Y9 Prime 2019 6,6 pulgadas LCD digitalizador de pantalla táctil + un conjunto de herramientas (negro)"
+});
+productList.push({
+  category: 'screens',
+  name: 'Yodoit Pantalla para iPhone 11',
+  price: 549,
+  image: 'https://m.media-amazon.com/images/I/61q9aXltpLL._AC_SL1200_.jpg',
+  definition: "Yodoit Pantalla para iPhone 11 Reemplazo Negro, 6.1 Pulgadas Display y Digitalizador Táctil Vidrio con Herramientas, Compatible con Modello A2111, A2221, A2223"
+});
+productList.push({
+  category: 'screens',
+  name: 'Yodoit Pantalla para Huawei ',
+  price: 599,
+  image: 'https://m.media-amazon.com/images/I/617GONNLEpS._AC_SL1200_.jpg',
+  definition: "Yodoit Pantalla para Huawei Y9 2019 / Enjoy 9 Plus, 6,5 LCD Táctil Digitalizador Cristal Reemplazo con Herramienta de reparación Negro"
+});
+productList.push({
+  category: 'screens',
+  name: 'Reemplazo de Pantalla Huawei',
+  price: 753,
+  image: 'https://m.media-amazon.com/images/I/61gySA55GeL._AC_SL1500_.jpg',
+  definition: "Reemplazo de Pantalla Huawei, Montaje de Digitalizador de Pantalla LCD Táctil 3D para Huawei para Nova 5T, para Honor 20 YAL-L21 L61A L61D L71A con Herramientas de Reparación Completas"
+});
+productList.push({
+  category: 'screens',
+  name: 'Pantalla LCD OEM para Huawei',
+  price: 1117,
+  image: 'https://m.media-amazon.com/images/I/61dIsVoejFL._AC_SL1100_.jpg',
+  definition: "Pantalla LCD OEM para Huawei Honor 20 / Nova 5T Digitizer Ensamblaje Completo con Marco"
+});
+// Glue
+productList.push({
+  category: 'glue',
+  name: 'ewrTM00sdQ Pegamento',
+  price: 139,
+  image: 'https://m.media-amazon.com/images/I/61N8AKWnpIL._SL1500_.jpg',
+  definition: "ewrTM00sdQ Pegamento calcomanía negro, sin olor, flexible, sin olor, T-7000, alta pureza, ecológico, 2 unidades, 15 ml"
 });
 productList.push({
   category: 'glue',
-  name: 'T7000 50ml Pegamento',
-  price: 120,
-  image: 'https://m.media-amazon.com/images/I/41c8hKlbvhL._SX425_.jpg',
+  name: 'B7000 Pegamento',
+  price: 781,
+  image: 'https://m.media-amazon.com/images/I/71KjakUWD-L._SL1500_.jpg',
+  definition: "B7000 Pegamento adhesivo de diamantes de imitación para manualidades, 2 piezas 110 ml / 3.7 fl oz B7000 Pegamento transparente con 5 bolígrafos de puntos, lápiz de cera y pinzas, pegamento de joyería para manualidades, maquillaje, zapatos, joyería, arte de uñas"
 });
 productList.push({
   category: 'glue',
-  name: 'B7000 50ml Pegamento',
-  price: 115,
-  image: 'https://m.media-amazon.com/images/I/31y6HFmlawL.jpg',
+  name: 'Eclectic 570110, transparente',
+  price: 300,
+  image: 'https://m.media-amazon.com/images/I/61cYJzUC4hL._AC_SL1000_.jpg',
+  definition: "Eclectic 570110, transparente 0.9 onzas líquidas E6000+Plus calcomanía multiusos, 0.9 onzas, 0.9 onzas"
 });
 productList.push({
-  category: 'cells',
-  name: 'Xiaomi Celular Redmi 12C',
-  price: 2048,
-  image: 'https://m.media-amazon.com/images/I/51cU7HevYPL._AC_SL1000_.jpg',
+  category: 'glue',
+  name: 'Flex Super Glue',
+  price: 249.,
+  image: 'https://m.media-amazon.com/images/I/81Ui5+UYyyL._SL1500_.jpg',
+  definition: "Flex Super Glue, Adhesivo en Gel de Secado Rápido Cianoacrilato, Alto Rendimiento, Fijacíon Super Fuerte, Durabilidad Extrema, Fijación Inmediata, Transparente, Botella de 15 gramos"
 });
 productList.push({
-  category: 'cells',
-  name: 'DOOGEE N50 2023',
-  price: 3204,
-  image: 'https://m.media-amazon.com/images/I/71455e5RWrL._AC_SL1500_.jpg',
+  category: 'glue',
+  name: 'Kit de 2 piezas de pegamento negro',
+  price: 480,
+  image: 'https://m.media-amazon.com/images/I/61gFHV5E+ML._SL1280_.jpg',
+  definition: "Kit de 2 piezas de pegamento negro T7000 de 110 ml para teléfono celular, computadora, electrónica, kit de herramientas de reparación adhesiva, caja de marco de visualización LCD de goma, tela de plástico, arte, manualidades, suministros de manualidades, impermeable, pegamento universal de precisión"
 });
 productList.push({
-  category: 'cells',
-  name: 'Samsung Galaxy S20 Plus',
-  price: 6499,
-  image: 'https://m.media-amazon.com/images/I/61kaZwuwdzL._AC_SL1200_.jpg',
+  category: 'glue',
+  name: 'T-7000 - Pegamento',
+  price: 1154,
+  image: 'https://m.media-amazon.com/images/I/619VnErU52L._SL1500_.jpg',
+  definition: "T-7000 - Pegamento apto para reparación, multiusos, de alto rendimiento, industrial, semilíquido, adhesivo negro (50 ml, 1 paquete)"
+});
+// Kits
+productList.push({
+  category: 'kits',
+  name: 'Kit de Reparación',
+  price: 224,
+  image: 'https://m.media-amazon.com/images/I/61eGDSi1SAL._AC_SL1500_.jpg',
+  definition: "Kit de Reparación de Electrónica, Smartphone, Computadora Y Tableta, Kit de Reparación de Herramienta de Palanca de Apertura de Electrónica Profesional para Mantenimiento de Electrónica"
 });
 productList.push({
-  category: 'cells',
-  name: 'SAMSUNG Galaxy M23',
-  price: 6999,
-  image: 'https://m.media-amazon.com/images/I/61nNowhLzwL._AC_SL1500_.jpg',
+  category: 'kits',
+  name: 'Herramientas de extracción',
+  price: 299,
+  image: 'https://m.media-amazon.com/images/I/61q+FB0XKKL._AC_SL1000_.jpg',
+  definition: "Juego de 10, kit de herramientas de extracción de recorte automático, kit de herramientas de palanca de instalador de extracción de radio del panel del coche findTop, kit de eliminación de tapicería, raspador de barra de palanca de removedor de sujetador - azul y rojo"
 });
 productList.push({
-  category: 'cells',
-  name: 'motorola Edge 30 Neo',
-  price: 6999,
-  image: 'https://m.media-amazon.com/images/I/61F2s51MtVL._AC_SL1500_.jpg',
+  category: 'kits',
+  name: 'Destornilladores de precisión',
+  price: 309,
+  image: 'https://m.media-amazon.com/images/I/71FI6aew5dL._AC_SL1128_.jpg',
+  definition: "Kit de Destornilladores de precisión 130 en 1 – AFujia Juego de Herramientas Magnéticas de Reparación Electrónica Profesional para iPhone, Computadora, Xbox Macbook PS4, PC"
 });
 productList.push({
-  category: 'cells',
-  name: 'Celular Poco X5 Pro 5G Black',
-  price: 5799,
-  image: 'https://m.media-amazon.com/images/I/71BJz1Hyz6L._AC_SL1000_.jpg',
+  category: 'kits',
+  name: 'Pinzas Precisión Kit',
+  price: 249.,
+  image: 'https://m.media-amazon.com/images/I/71TxKEynq7L._AC_SL1000_.jpg',
+  definition: "PUBUNUS Pinzas Precisión Kit, 9 Piezas Pinzas ESD Conjunto, Anti-estática de Aacero Inoxidable, Pinzas no Magnéticas Pinzas Fijadas para Artesanía, Joyería, Electrónica, Trabajo de Laboratorio, Ceja y Pelo Encarnado"
+});
+productList.push({
+  category: 'kits',
+  name: 'Pinzas de Precision',
+  price: 157.,
+  image: 'https://m.media-amazon.com/images/I/51yoNRjHA7L._AC_.jpg',
+  definition: "Bauihr Pinzas de Precision, 9 Piezas Pinzas ESD Conjunto, Antiácidas Tweezers de Acero Inoxidable Para Trabajos de Laboratorio, Electrónica, Manualidades, Joyería, Soldadura"
+});
+productList.push({
+  category: 'kits',
+  name: 'Destornilladores de precisión',
+  price: 149,
+  image: 'https://m.media-amazon.com/images/I/61D6JGq-JKL._AC_SL1200_.jpg',
+  definition: "Kit de Destornilladores de precisión 25 en 1 – AFujia Juego de Herramientas de Reparación Electrónica Profesional con Caja Portátil para iPhone, Computadora, Xbox Macbook PS4, PC"
 });
 
 // ######################################### addEventListeners #########################################
@@ -150,8 +264,8 @@ productList.push({
 // menu hamburgesa icon
 menuHamIcon.addEventListener('click', mobilFunction);
 // desktop menus
-desktopAll[0].addEventListener("click", clearAllproducts);
-desktopAll[1].addEventListener("click", clearAllproducts);
+desktopAll[0].addEventListener("click", origin);
+desktopAll[1].addEventListener("click", origin);
 
 desktopCells[0].addEventListener("click", origin);
 desktopCells[1].addEventListener("click", origin);
@@ -248,14 +362,15 @@ function delayInactive(){
 }
 // main-container
 function productDetailsOpen(event){
-
   const imgSelect = event.target.currentSrc;
   const price = event.target.parentElement.childNodes[1].childNodes[0].childNodes[0].childNodes[1].innerText;
   const title = event.target.parentElement.childNodes[1].childNodes[0].childNodes[1].innerText;
-
+  const description = event.target.parentElement.childNodes[1].childNodes[0].childNodes[2].innerText
+ 
   imgSrc.setAttribute("src", imgSelect);
   nameTitle.innerText = title;
   priceDetail.innerText = price;
+  descriptionDetail.innerHTML = description;
 
   addToCartButton.addEventListener("click", addToCart);
   productDetalItem.classList.remove("inactive");
@@ -263,7 +378,6 @@ function productDetailsOpen(event){
   setOnDark();
   delayBackground()
   body.classList.add("scroll-off");
-
 }
 function addToCart(event){
   let imgCar;
@@ -357,7 +471,7 @@ function clear(event){
 }
 // render trigers
 function origin(event){
-  console.log(event);
+  closeAll();
   if(event.target.className == "desktop-all"){
     renderProducts(productList, 'all');
   }
@@ -376,17 +490,12 @@ function origin(event){
   else if(event.target.className == "desktop-kits"){
     renderProducts(productList, 'kits');
   }
-
-  if(event.target.firstChild){
-    closeAll();
-    console.log("cerrando");
-  }
 }
 // render function
 function renderProducts(arr, category) {
-  
+  clearAllproducts();
   for (product of arr) {
-    if(product.category == category){
+    if(product.category == category || category == "all"){
       const productImgCart = document.createElement('img');
       productImgCart.setAttribute('id', 'cart-img');
       productImgCart.setAttribute('src', './icons/cart.svg');
@@ -394,6 +503,12 @@ function renderProducts(arr, category) {
       const productInfoFigure = document.createElement('figure');
       productInfoFigure.appendChild(productImgCart);
   
+      const productDefinition = document.createElement('p');
+      productDefinition.innerText = product.definition;
+      const productDefinitionDiv = document.createElement('div');
+      productDefinitionDiv.setAttribute("class", "product-definition")
+      productDefinitionDiv.appendChild(productDefinition);
+
       const productName = document.createElement('p');
       productName.innerText = product.name;
       const productDescriptionDiv = document.createElement('div');
@@ -410,21 +525,21 @@ function renderProducts(arr, category) {
       
       const productTextcontainer = document.createElement('div');
       productTextcontainer.setAttribute("class", "product-text-container")
-      productTextcontainer.append(productInfoDiv, productDescriptionDiv);
+      productTextcontainer.append(productInfoDiv, productDescriptionDiv,productDefinitionDiv);
   
       const productInfo = document.createElement('div');
-      productInfo.classList.add('product-info');
+      productInfo.setAttribute("class", 'product-info');
       productInfo.append(productTextcontainer, productInfoFigure);
   
       const productImg = document.createElement('img');
       productImg.setAttribute('src', product.image);
       productImg.addEventListener('click', productDetailsOpen);
   
-      const productCard = document.createElement('div');
-      productCard.classList.add('product-card');
-      productCard.append(productImg, productInfo);
+      const productCardContainer = document.createElement('div');
+      productCardContainer.setAttribute("class", 'product-card');
+      productCardContainer.append(productImg, productInfo);
   
-      cardsContainer.appendChild(productCard);
+      cardsContainer.appendChild(productCardContainer);
     }
   }
 }
@@ -434,13 +549,7 @@ function productDetailsClose(){
   productDetalItem.classList.remove("active");
   setOffDark();
 }
-function clearAllproducts(event){
-  console.log(productCard);
-  
-  //cardsContainer.remove(productCard);
-  //productCard.remove("all");
-  //cardsContainer.removeChild(productCard[*]);
+function clearAllproducts(){
+  cardsContainer.innerHTML = "";
 }
-
-
 checkCart();
